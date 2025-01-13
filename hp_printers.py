d66a9e8e72d389a26a512e46a21487e87647a38f
Will <wgnann@gmail.com>
2025-01-13 11:12:53 -0300

    Commit inicial.

---
+++ hp_printers.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+requests.packages.urllib3.disable_warnings()
+
+def page_count(printer_ip):
+    url = "https://"+printer_ip+"/hp/device/InternalPages/Index?id=UsagePage"
+    page = requests.get(url, verify=False)
+    parser = BeautifulSoup(page.content, features="lxml")
+    element = parser.find("td", {"id": "UsagePage.EquivalentImpressionsTable.Total.Total"}, "html.parser")
+    return element.getText()
+
+if __name__ == "__main__":
+    from sys import argv
+    ip = argv[1]
+    print(page_count(ip))
